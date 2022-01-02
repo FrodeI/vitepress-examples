03e74560f05ac941c533329c36fed5683aa72fc3
Frode Ingebrigtsen <FrodeI@users.noreply.github.com>
2022-01-02 18:10:50 +0100

    Added example with external link in sidebar

---
+++ external-sidebar-links/.vitepress/config.ts
@@ -0,0 +1,13 @@
+export default {
+  lang: 'en-US',
+  title: 'External sidebar links',
+
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+
+    sidebar: [
+      { text: 'Internal', link: '/internal' },
+      { text: 'External', link: 'https://github.com/FrodeI/vitepress-examples' }
+    ]
+  }
+}
